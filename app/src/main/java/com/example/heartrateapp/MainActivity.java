20aad0adb44e866ac06f97b878bdb4ed6d8a748d
MabZyraf <w.gierdziejeski@gmail.com>
2026-01-08 23:34:53 +0100

    test wiktor

--- app/src/main/java/com/example/heartrateapp/MainActivity.java
+++ app/src/main/java/com/example/heartrateapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
+//test wiktor
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
